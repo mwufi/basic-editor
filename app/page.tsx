aa7902c46604365b643cb244be70375c584b35c9
Zen <ztang230@gmail.com>
2024-08-15 20:49:04 -0500

    doc saves good

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,6 @@ import Tiptap from "@/components/TipTap";
 
 export default function Home() {
   return (
-    <div className="h-full p-8">
-      <Tiptap editable={true} font="serif" />
-    </div>
+    <Tiptap editable={true} font="serif" />
   );
 }
--- components/TipTap.tsx
+++ components/TipTap.tsx
@@ -25,7 +25,50 @@ const Center = ({ children }: { children: React.ReactNode }) => {
     return <div className='relative h-full'><div className="mx-auto max-w-[600px] py-6">{children}</div></div>
 }
 
-const Tiptap = ({ editable = true, font = 'serif', wordcount = false }) => {
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { useState } from "react"
+import { Save, Share } from "lucide-react"
+
+const TopBar = ({ onSave, onShare, isEditing, title, setTitle, handleRetitle }) => {
+
+
+    return (
+        <div className="sticky top-0 z-10 flex items-center justify-center p-2 rounded">
+            {true ? (
+                <form onSubmit={(e) => {
+                    e.preventDefault();
+                    handleRetitle();
+                }} className='h-12'>
+                    <Input
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
+                        className='w-[600px] text-lg font-semibold text-center focus-visible:ring-0 outline-none border-none hover:bg-orange-100'
+                        placeholder='Untitled'
+                        onBlur={handleRetitle}
+                        autoFocus
+                    />
+                </form>
+            ) : (
+                <h1 className="text-lg font-semibold h-12" onClick={handleRetitle}>
+                    {title}
+                </h1>
+            )}
+            <div className="absolute right-0">
+                <Button size="sm" variant="ghost" onClick={onSave}>
+                    <Save className="mr-2 h-4 w-4" />
+                    Save
+                </Button>
+                <Button size="sm" variant="ghost" onClick={onShare}>
+                    <Share className="mr-2 h-4 w-4" />
+                    Share
+                </Button>
+            </div>
+        </div>
+    )
+}
+
+const Tiptap = ({ editable = true, font = 'serif', wordcount = true }) => {
     const editor = useEditor({
         extensions: [StarterKit, CharacterCount],
         content: '<p>Hello World! 🌎️</p>',
@@ -37,38 +80,48 @@ const Tiptap = ({ editable = true, font = 'serif', wordcount = false }) => {
         editable: editable,
     })
 
+    const [title, setTitle] = useState("Untitled")
+    const [isEditing, setIsEditing] = useState(false)
+
+    const handleRetitle = () => {
+        if (isEditing) {
+            setIsEditing(false)
+        } else {
+            setIsEditing(true)
+        }
+    }
 
     return (
-        <Center>
-            <div className="absolute top-0 left-0">
-                {wordcount && <CharacterCountMarker
-                    current={editor?.storage.characterCount.words()}
-                    limit={500}
-                    display="words"
-                />}
-                <SaveDialog onSave={(title) => {
+        <div className="flex flex-col px-6">
+            <TopBar
+                isEditing={isEditing}
+                title={title}
+                setTitle={setTitle}
+                handleRetitle={handleRetitle}
+                onSave={() => {
                     const notesManager = new IndexedDBNotesManager();
                     notesManager.addNote({
                         title: title,
                         content: editor?.getHTML() ?? '',
                     });
+                }} onShare={() => {
+                    navigator.clipboard.writeText(editor?.getHTML() ?? '')
                 }} />
-                {/* <button className="bg-gray-200 p-2 rounded-md" onClick={() => {
-                    // navigator.clipboard.writeText(editor?.getHTML() ?? '')
-                    const notesManager = new IndexedDBNotesManager();
-                    notesManager.addNote({
-                        title: 'Untitled',
-                        content: editor?.getHTML() ?? '',
-                    });
-                }}>
-                    Save
-                </button> */}
+
+            <div className="fixed left-4 bottom-4">
+                {wordcount && <CharacterCountMarker
+                    current={editor?.storage.characterCount.words()}
+                    limit={500}
+                    display="words"
+                />}
             </div>
-            <EditorContent
-                editor={editor}
-                className="h-full [&_.ProseMirror-focused]:caret-[#4b494b] [&_.ProseMirror-focused]:caret-[4px]"
-            />
-        </Center>
+            <Center>
+                <EditorContent
+                    editor={editor}
+                    className="h-full [&_.ProseMirror-focused]:caret-[#4b494b] [&_.ProseMirror-focused]:caret-[4px]"
+                />
+            </Center>
+        </div>
 
     )
 }
