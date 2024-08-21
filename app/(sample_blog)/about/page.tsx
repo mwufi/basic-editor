0e6d82572915ee887df3f1765afbb8464e5becf1
Zen <ztang230@gmail.com>
2024-08-21 13:40:47 -0500

    about page

--- app/(sample_blog)/about/page.tsx
+++ app/(sample_blog)/about/page.tsx
@@ -1,7 +1,15 @@
+import BackgroundImageHeader from "@/components/blocks/BackgroundImageHeader";
+
 export default function About() {
     return (
         <div>
-            <h1>About</h1>
+            <BackgroundImageHeader height="h-96">
+                <div className="flex flex-col justify-end h-full pb-12 px-4 max-w-3xl mx-auto">
+                    <h1 className="text-6xl font-bold text-white leading-tight mb-4 max-w-3xl">
+                        About
+                    </h1>
+                </div>
+            </BackgroundImageHeader>
         </div>
     )
 }
--- app/(sample_blog)/layout.tsx
+++ app/(sample_blog)/layout.tsx
@@ -1,44 +1,5 @@
 import React from "react"
-import { NavLink } from "@/components/blocks/NavLink"
-
-type NavItem = {
-    href: string
-    name: string
-}
-
-function Nav({ items }: { items: NavItem[] }) {
-    return (
-        <nav className="border-b">
-            <ul className="flex space-x-8 mx-auto max-w-3xl p-4">
-                {items.map((item, index) =>
-                    item.href.startsWith("http") ? (
-                        <li key={index}>
-                            <a
-                                href={item.href}
-                                target="_blank"
-                                rel="noopener noreferrer"
-                                className="text-gray-600 font-medium"
-                            >
-                                {item.name}
-                            </a>
-                        </li>
-                    ) : (
-                        <li key={index}>
-                            <NavLink
-                                href={item.href}
-                                exact
-                                className="text-gray-600 font-medium transition-colors duration-300"
-                            >
-                                {item.name}
-                            </NavLink>
-                        </li>
-                    )
-                )}
-            </ul>
-        </nav>
-    )
-}
-
+import Nav from "@/components/blocks/TopNav"
 const BlogLayout = ({ children }: { children: React.ReactNode }) => {
 
     const navItems = [
