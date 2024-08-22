8fb5db8a6f25753c2fedcbf41be4ec0b012d862c
Zen <ztang230@gmail.com>
2024-08-22 14:48:45 -0500

    k

--- lib/instantdb/client.ts
+++ lib/instantdb/client.ts
@@ -5,19 +5,35 @@ import { init, tx, id } from '@instantdb/react'
 // ID for app: timothy
 const APP_ID = '71186fbd-29c5-4d5f-bc04-68f6120e63c1'
 
-type BlogPost = {
-    id: string;
-    title: string;
-    subtitle: string;
-    author: string;
-    content: string;
-    createdAt: number;
-    likes: number;
-}
-
-// Optional: Declare your schema for intellisense!
+// Schema definition based on the provided data model
 type Schema = {
-    blogPosts: BlogPost;
-}
+  users: {
+    id: string;
+    email: string;
+    handle: string;
+    createdAt: number;
+    posts: Array<Schema['posts']>;
+    pin: Schema['pins'];
+  };
+  posts: {
+    id: string;
+    text: string;
+    createdAt: number;
+    comments: Array<Schema['comments']>;
+    author: Schema['users'];
+    pin: Schema['pins'];
+  };
+  comments: {
+    id: string;
+    text: string;
+    post: Schema['posts'];
+    author: Schema['users'];
+  };
+  pins: {
+    id: string;
+    post: Schema['posts'];
+    author: Schema['users'];
+  };
+};
 
 export const db = init<Schema>({ appId: APP_ID })
