77ab92c1322e5106bebbbcf063c468da42f0754c
Zen <ztang230@gmail.com>
2024-08-21 22:50:10 -0500

    hm..

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import BackgroundImageHeader from "@/components/blocks/BackgroundImageHeader";
+import TopNav from "@/components/blocks/TopNav";
 import ReadOnlyEditor from "@/components/editor/ReadOnlyEditor";
 import { AboutText } from "@/components/WelcomeText";
 
@@ -14,8 +15,15 @@ export default function About() {
                     </h1>
                 </div>
             </BackgroundImageHeader>
-            <div className="max-w-3xl mx-auto p-4 mt-12">
-                <ReadOnlyEditor content={AboutText} font="serif" />
+            <div className="max-w-3xl mx-auto p-4">
+                <TopNav items={[
+                    { href: "/", name: "Home" },
+                    { href: "/about", name: "About" },
+                    { href: "/new", name: "New Post" },
+                ]} />
+                <div className="mt-12">
+                    <ReadOnlyEditor content={AboutText} font="serif" />
+                </div>
             </div>
         </div>
     )
