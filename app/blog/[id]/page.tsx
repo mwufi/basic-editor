54b1404b422977289b29afced94cdafd86961cac
Zen <ztang230@gmail.com>
2024-08-23 14:41:32 -0500

    looks slightly better

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -28,7 +28,7 @@ const BlogPost = () => {
     return (
         <div className="overflow-y-scroll min-h-screen w-full">
             <main className="max-w-3xl mx-auto p-4">
-                <h1 className="text-3xl font-bold mb-4">{post.title}</h1>
+                <h1 className="text-4xl font-bold my-14">{post.title}</h1>
                 <p className="text-gray-500 mb-4">
                     {new Date(post.createdAt).toLocaleDateString()}
                 </p>
