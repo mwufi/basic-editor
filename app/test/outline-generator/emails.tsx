9c5c1f78177fc53a918503ec2740b85e776d4fe4
Zen <ztang230@gmail.com>
2024-08-26 18:41:02 -0500

    rm expander

--- app/test/outline-generator/emails.tsx
+++ app/test/outline-generator/emails.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Search } from "lucide-react";
